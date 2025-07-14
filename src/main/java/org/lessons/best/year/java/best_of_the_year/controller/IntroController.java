30092c4e59caae96b6379c9d903da54fbda4ce16
erikaCutugno <erikucciapop@gmail.com>
2025-07-14 20:27:31 +0200

    add step 3

--- src/main/java/org/lessons/best/year/java/best_of_the_year/controller/IntroController.java
+++ src/main/java/org/lessons/best/year/java/best_of_the_year/controller/IntroController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -41,6 +42,38 @@ public class IntroController {
         model.addAttribute("category", "Songs");
         model.addAttribute("title", title);
         return "stringList";
+    }
+    @GetMapping("/movies/{id}")
+    public String movieById(Model model, @PathVariable("id") Integer movieId) {
+        Movie movieFound = null;
+
+        for (Movie movie : getBestMovies()) {
+            if (movie.getId().equals(movieId)) {
+                movieFound = movie;
+                break;
+            }
+        }
+
+        model.addAttribute("id", movieId);
+        model.addAttribute("title", movieFound.getTitle());
+        model.addAttribute("category", "Movies");
+        return "pageDetails";
+    }
+    @GetMapping("/songs/{id}")
+    public String songById(Model model, @PathVariable("id") Integer songId) {
+        Song songFound = null;
+
+        for (Song song : getBestSongs()) {
+            if (song.getId().equals(songId)) {
+                songFound = song;
+                break;
+            }
+        }
+
+        model.addAttribute("id", songId);
+        model.addAttribute("title", songFound.getTitle());
+        model.addAttribute("category", "Songs");
+        return "pageDetails";
     }
    private List<Movie> getBestMovies() {
         List<Movie> list = new ArrayList<Movie>();
